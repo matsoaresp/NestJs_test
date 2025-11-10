40a3de340e857ac01d9cb638ed75259bde1f5481
Mateus Prado <maateussp@gmail.com>
2025-11-10 12:19:59 -0300

    Initialize new Repository

---
+++ src/users/app.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { AppService } from '../users/app.service';
+import type { User } from '../interfaces/app.userinterface';
+
+@Controller('users')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('all')
+  getUsers() {
+    return this.appService.getUsers();
+  }
+
+  @Post('create')
+  createUser(@Body() newUser: User) {
+    this.appService.createUser(newUser);
+    return { message: 'Usuario criado com sucesso! ' };
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: number) {
+    const user = this.appService.getUserById(+id);
+    if (!user) {
+      return { message: 'User not found' };
+    } else {
+      return user;
+    }
+  }
+  @Post('delete/:id')
+  deleteUser(@Param('id') id: number) {
+    this.appService.deleteUsersById(id);
+    return { message: 'User deleted successfully' };
+  }
+}
---
+++ src/users/app.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '../interfaces/app.userinterface';
+
+@Injectable()
+export class AppService {
+  private readonly users: User[] = [];
+
+  getHello(): string {
+    return 'Seja bem vindo ao NestJS!';
+  }
+
+  getUsers(): User[] {
+    return this.users;
+  }
+
+  createUser(user: User): void {
+    this.users.push(user);
+  }
+
+  getUserById(id: number): User | undefined {
+    return this.users.find((user) => user.id === id);
+  }
+
+  deleteUsersById(id: number): void {
+    const index = this.users.findIndex((user) => user.id === id);
+    if (index !== -1) {
+      this.users.splice(index, 1);
+    }
+  }
+}
